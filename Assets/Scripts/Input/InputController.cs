e5d6bad82e3daa6df3e2d0979ba77df6297c7913
ph7-5672 <51991906+ph7-5672@users.noreply.github.com>
2023-02-04 13:14:33 +0800

    【鼠标控制节点旋转和交换】

---
+++ Assets/Scripts/Input/InputController.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    private BaseCircuitComponent selectedNode;
+
+    private Color originColor;
+    
+    
+    private void Update()
+    {
+        ClickNodeUpdate();
+        SwapNodeUpdate();
+    }
+
+    /// <summary>
+    /// 单击元件旋转。
+    /// </summary>
+    void ClickNodeUpdate()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            var node = GetNodeByMousePosition();
+            if (node == null)
+            {
+                return;
+            }
+            node.RotateClockwise();
+            UnSelectNode();
+        }
+        
+    }
+
+    /// <summary>
+    /// 元件交换位置。
+    /// </summary>
+    void SwapNodeUpdate()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            var node = GetNodeByMousePosition();
+            if (node == null)
+            {
+                return;
+            }
+
+            if (selectedNode == null)
+            {
+                SelectNode(node);
+            }
+            else if (selectedNode.Equals(node))
+            {
+                UnSelectNode();
+            }
+            else
+            {
+                selectedNode.Swap(node);
+            }
+        }
+
+    }
+
+    void SelectNode(BaseCircuitComponent node)
+    {
+        selectedNode = node;
+        var sr = selectedNode.GetComponent<SpriteRenderer>();
+        originColor = sr.color;
+        sr.color = Color.gray;
+    }
+
+
+    void UnSelectNode()
+    {
+        if (selectedNode == null)
+        {
+            return;
+        }
+
+        var sr = selectedNode.GetComponent<SpriteRenderer>();
+        sr.color = originColor;
+        selectedNode = null;
+    }
+
+    BaseCircuitComponent GetNodeByMousePosition()
+    {
+        // 获取鼠标位置。
+        var mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var c = Physics2D.OverlapPoint(mp);
+        if (c != null)
+        {
+            var circuit = c.GetComponent<BaseCircuitComponent>();
+            if (circuit.ComponentType == Constants.ComponentType.NODE)
+            {
+                return circuit;
+            }
+        }
+        return null;
+    }
+
+}
