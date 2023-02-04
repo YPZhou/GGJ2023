2abeb0a8532eac1c660bf6dd0039dd9223bb3ac6
YPZhou <yunpeng_zhou@163.com>
2023-02-04 22:38:16 +0800

    添加节点转动动画

--- Assets/Scripts/Data/CircuitComponent/BaseCircuitComponent.cs
+++ Assets/Scripts/Data/CircuitComponent/BaseCircuitComponent.cs
@@ -16,10 +16,27 @@ public abstract class BaseCircuitComponent : MonoBehaviour
 		SetupConnectedTiles();
 	}
 
-	protected void Update()
+	protected void Start()
 	{
 		transform.localPosition = bottomLeftPosition + new Vector3(TileCoords.Item1 - 1, TileCoords.Item2 - 1);
 		transform.localRotation = Quaternion.AngleAxis(90 * rotationCount, Vector3.back);
+
+		previousRotationCount = rotationCount;
+	}
+
+	protected void Update()
+	{
+		transform.localPosition = bottomLeftPosition + new Vector3(TileCoords.Item1 - 1, TileCoords.Item2 - 1);
+
+		if (previousRotationCount != rotationCount)
+		{
+			transform.localRotation = Quaternion.AngleAxis(90 * previousRotationCount + 360 * (Time.time - rotationStartTime), Vector3.back);
+
+			if (Time.time - rotationStartTime >= 0.25f)
+			{
+				previousRotationCount = rotationCount;
+			}
+		}
 
 		UpdateCore();
 	}
@@ -44,6 +61,8 @@ public abstract class BaseCircuitComponent : MonoBehaviour
 
 	public Tuple<int, int> TileCoords { get; protected set; }
 
+	float rotationStartTime = 0f;
+	int previousRotationCount = 0;
 	int rotationCount = 0;
 
 	protected IEnumerable<Tuple<int, int>> CalculatedConnectedTiles => ConnectedTiles.Select(tile =>
@@ -65,13 +84,14 @@ public abstract class BaseCircuitComponent : MonoBehaviour
 
 	public void RotateClockwise()
 	{
+		previousRotationCount = rotationCount;
 		rotationCount += 1;
 		if (rotationCount > 3)
 		{
 			rotationCount = 0;
 		}
 
-		
+		rotationStartTime = Time.time;
 	}
 
 	public void Swap(BaseCircuitComponent otherComponent)
