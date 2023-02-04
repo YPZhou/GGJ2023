0839238d110c831cd05df6d4b82354588772c7e5
YPZhou <yunpeng_zhou@163.com>
2023-02-04 09:07:27 +0800

    添加关卡场景及控制脚本

---
+++ Assets/Scripts/SceneController/GameTitleController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameTitleController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SceneManager.LoadScene("Level1");
+		}
+	}
+}
